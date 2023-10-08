7c2145f0212900c4573a3416c4217e7b21c41970
Brenno Rodrigues <brennopereira6@gmail.com>
2023-10-08 04:54:50 -0300

    feat: adding eval module

--- app/Eval.hs
+++ app/Eval.hs
@@ -1,6 +1,74 @@
-module Eval (evaluate) where
+module Eval (eval) where
 
-import Parser (Expr)
+import Data.Map as Map
+import Parser (Expr (..))
 
-evaluate :: Expr -> IO()
-evaluate _ = print("")
+data Value = A Integer | B String | C Context [String] Expr deriving (Show)
+
+type Context = Map String Value
+
+checkBuiltInValues :: String -> [Expr] -> Context -> IO Value
+checkBuiltInValues name args ctx = case name of
+  "+" -> do
+    f <- evalExpr ctx $ head args
+    s <- evalExpr ctx $ head $ tail args
+
+    case (f, s) of
+        (A v, A v') -> return $ A $ v + v'
+        _ -> error "Sum with invalid params"
+  "-" -> do
+    f <- evalExpr ctx $ head args
+    s <- evalExpr ctx $ head $ tail args
+
+    case (f, s) of
+        (A v, A v') -> return $ A $ v - v'
+        _ -> error "Sum with invalid params"
+  "*" -> do
+    f <- evalExpr ctx $ head args
+    s <- evalExpr ctx $ head $ tail args
+
+    case (f, s) of
+        (A v, A v') -> return $ A $ v * v'
+        _ -> error "Sum with invalid params"
+  "/" -> do
+    f <- evalExpr ctx $ head args
+    s <- evalExpr ctx $ head $ tail args
+
+    case (f, s) of
+        (A v, A v') -> return $ A $ v `div` v'
+        _ -> error "Sum with invalid params"
+  _ -> error $ show ("Reference not found", name)
+
+evalExpr :: Context -> Expr -> IO Value
+evalExpr ctx expr = case expr of
+  Assignment name value next -> do
+    expr' <- evalExpr ctx value
+    let customCtx = Map.insert name expr' ctx
+    evalExpr customCtx next
+  Call name args -> case Map.lookup name ctx of
+    Just (A value) -> return $ A value
+    Just (B value) -> return $ B value
+    Just (C ctx' args' expr') -> do
+      args'' <- mapM (evalExpr ctx) args
+      let args''' = zip args' args''
+      let ctx'' = Prelude.foldl (flip $ uncurry Map.insert) ctx' args'''
+      evalExpr ctx'' expr'
+    Nothing -> checkBuiltInValues name args ctx
+  Var name -> case Map.lookup name ctx of
+    Just value -> return value
+    Nothing -> error $ show ("Reference not found", name)
+  Numeric value -> return $ A value
+  Text value -> return $ B value
+  Print args -> do
+    s <- sequence args'
+    print s
+    head args'
+    where
+      args' = Prelude.map (evalExpr ctx) args
+  Abs args expr' -> return $ C ctx args expr'
+  EOF -> error "Error on evalutation"
+
+eval :: Expr -> IO String
+eval expr = do
+  result <- evalExpr Map.empty expr
+  return $ show result
--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,29 @@ module Main where
 
 import Lexer (tokenize)
 import Parser (parse)
--- import Evaluate (evaluate)
+import Eval (eval)
+
+--import LuaPrinter (printToLua)
 
 source :: String
-source = unlines [
-        "let a = + 1 2;",
-        "+ a 4"
+source =
+  unlines
+    [ "let a = + 1 2;",
+      "+ a 4"
     ]
 
 main :: IO ()
 main = do
-    print $ tokenize $ init source
-    print $ parse $ tokenize $ init source
-    putStrLn "\n---------------\n"
-    print $ tokenize "let a = fn x y => + x y;\nlet c = (a 1 2);\nprint c"
-    print $ parse $ tokenize "let a = fn x y => + x y;\nlet c = (a 1 2);\nprint c"
+  -- putStrLn "\n---------------\n"
+  -- print $ tokenize "let a = fn x y => + (x) (y);\nlet c = (a 1 2);\nprint c"
+  -- print '\n'
+  -- print $ parse $ tokenize "let a = fn x y => + (x) (y);\nlet c = a 1 2 3 (b 1 2);\nprint c"
+  -- print "\n lua:"
+  -- print $ printToLua f
+  print f
+  print "\n evaluation:"
+  result <- eval f
+  print "\n evaluation return:"
+  print result
+  where
+    (f, _) = parse $ tokenize "let a = fn x y => + x y;\nlet b = print (* 3 (a 1 2)); print b \"wasdwasd\""
