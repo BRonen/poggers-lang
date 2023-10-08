97865d48cc1596806633d176bcfd9d7be8e3ff55
Brenno Rodrigues <brennopereira6@gmail.com>
2023-10-08 04:52:48 -0300

    refactor: removing builtin operators from parser and refactor lol

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -1,64 +1,60 @@
 module Lexer (tokenize, Token (..)) where
 
-import Data.Char(isDigit)
-import Text.Read(readMaybe)
+import Data.Char (isDigit)
+import Text.Read (readMaybe)
 
-data Token =
-    TextToken String
-    | LiteralToken String
-    | NumericToken Integer
-    | PrintToken
-    | Let
-    | FunctionDef
-    | FatArrow
-    | Plus
-    | Minus
-    | Slash
-    | Star
-    | Equal
-    | LParen
-    | RParen
-    | BreakExpr
-        deriving (Show, Eq)
+data Token
+  = TextToken String
+  | LiteralToken String
+  | NumericToken Integer
+  | PrintToken
+  | Let
+  | FunctionDef
+  | FatArrow
+  | Plus
+  | Minus
+  | Slash
+  | Star
+  | Equal
+  | LParen
+  | RParen
+  | BreakExpr
+  deriving (Show, Eq)
 
 especialCharacters :: [Char]
-especialCharacters = ['(', ')', ';', '+', '-', '/', '*']
+especialCharacters = ['(', ')', ';']
 
 -- TODO: remove duplicated code
 getWordsFromSource :: [Char] -> [String] -> String -> [String]
-getWordsFromSource acc wordsAcc [] = (wordsAcc ++ ([acc]))
-getWordsFromSource [] wordsAcc (x:xs)
-    | elem x especialCharacters = getWordsFromSource [] (wordsAcc ++ ([[x]])) xs
-    | x == ' ' || x == '\n' = getWordsFromSource [] wordsAcc xs
-    | otherwise = getWordsFromSource [x] wordsAcc xs
-getWordsFromSource acc wordsAcc (x:xs)
-    | elem x especialCharacters = getWordsFromSource [] (wordsAcc ++ ([acc]) ++ ([[x]])) xs
-    | x == ' ' || x == '\n' = getWordsFromSource [] (wordsAcc ++ ([acc])) xs
-    | otherwise = getWordsFromSource (acc++([x])) wordsAcc xs
+getWordsFromSource acc wordsAcc [] = wordsAcc ++ [acc]
+getWordsFromSource [] wordsAcc (x : xs)
+  | x `elem` especialCharacters = getWordsFromSource [] (wordsAcc ++ [[x]]) xs
+  | x == ' ' || x == '\n' = getWordsFromSource [] wordsAcc xs
+  | otherwise = getWordsFromSource [x] wordsAcc xs
+getWordsFromSource acc wordsAcc (x : xs)
+  | x `elem` especialCharacters = getWordsFromSource [] (wordsAcc ++ [acc] ++ [[x]]) xs
+  | x == ' ' || x == '\n' = getWordsFromSource [] (wordsAcc ++ [acc]) xs
+  | otherwise = getWordsFromSource (acc ++ [x]) wordsAcc xs
 
 tokenizeLiteral :: String -> Token
 tokenizeLiteral word
-    | all isDigit word = case (readMaybe word :: Maybe Integer) of
-        Just value -> NumericToken value
-        Nothing    -> LiteralToken word
-    | head word == '"' && last word == '"' = TextToken $ tail $ init word
-    | otherwise = LiteralToken word
+  | all isDigit word = case (readMaybe word :: Maybe Integer) of
+      Just value -> NumericToken value
+      Nothing -> LiteralToken word
+  | head word == '"' && last word == '"' = TextToken $ tail $ init word
+  | otherwise = LiteralToken word
 
 getTokenByWord :: [Char] -> Token
 getTokenByWord word = case word of
-    "print" -> PrintToken
-    "let" -> Let
-    "fn" -> FunctionDef
-    "=>" -> FatArrow
-    "="   -> Equal
-    "("   -> LParen
-    ")"   -> RParen
-    ";"   -> BreakExpr
-    "+"   -> Plus
-    "-"   -> Minus
-    "/"   -> Slash
-    "*"   -> Star
-    _     -> tokenizeLiteral word
+  "print" -> PrintToken
+  "let" -> Let
+  "fn" -> FunctionDef
+  "=>" -> FatArrow
+  "=" -> Equal
+  "(" -> LParen
+  ")" -> RParen
+  ";" -> BreakExpr
+  _ -> tokenizeLiteral word
 
 tokenize :: String -> [Token]
 tokenize source = map getTokenByWord $ getWordsFromSource [] [] source
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,21 +1,17 @@
-module Parser (parse, Expr (..), Operator (..)) where
+module Parser (parse, Expr (..)) where
 
 import Lexer (Token (..))
 
-data Operator = Add
-    | Sub
-    | Div
-    | Mul
-        deriving (Show)
-data Expr = Assignment String Expr Expr
-    | Abs [String] Expr
-    | Print [Expr]
-    | Operation Operator Expr Expr
-    | Var String
-    | Call String [Expr]
-    | Numeric Integer
-    | Text String
-        deriving (Show)
+data Expr
+  = Assignment String Expr Expr
+  | Call String [Expr]
+  | Abs [String] Expr
+  | Print [Expr]
+  | Var String
+  | Numeric Integer
+  | Text String
+  | EOF
+  deriving (Show)
 
 parseValue :: Token -> [Token] -> (Expr, [Token])
 parseValue (TextToken value) tokens = (Text value, tokens)
@@ -23,63 +19,70 @@ parseValue (NumericToken value) tokens = (Numeric value, tokens)
 parseValue (LiteralToken value) tokens = (Var value, tokens)
 parseValue token _ = error $ show ("Invalid token passed to value parser", token)
 
-parseOperator :: Operator -> [Token] -> (Expr, [Token])
-parseOperator op tokens = (Operation op f f', s')
-    where
-        (f', s') = parse s
-        (f, s) = parse tokens
-
 parseArguments :: [String] -> [Token] -> ([String], [Token])
-parseArguments acc ((LiteralToken argument):tokens) = ([argument] ++ argumentsAcc, remainingTokens)
-    where (argumentsAcc, remainingTokens) = parseArguments acc tokens
-parseArguments acc ((FatArrow):tokens) = (acc, tokens)
+parseArguments acc ((LiteralToken argument) : tokens) = (argument : argumentsAcc, remainingTokens)
+  where
+    (argumentsAcc, remainingTokens) = parseArguments acc tokens
+parseArguments acc (FatArrow : tokens) = (acc, tokens)
 parseArguments _ tokens = error $ show ("Invalid function definition", tokens)
 
 parseFunction :: [Token] -> (Expr, [Token])
 parseFunction tokens = (Abs arguments body, remainingTokens')
-    where
-        (body, remainingTokens') = parse remainingTokens
-        (arguments, remainingTokens) = parseArguments [] tokens
+  where
+    (body, remainingTokens') = parse remainingTokens
+    (arguments, remainingTokens) = parseArguments [] tokens
 
 parseVariable :: [Token] -> (Expr, [Token])
-parseVariable ((LiteralToken name):(Equal):tokens) = (Assignment name expr expr', remainingTokens')
-    where
-        (expr', remainingTokens')= parse remainingTokens
-        (expr, remainingTokens) = parse tokens
+parseVariable ((LiteralToken name) : Equal : tokens) = (Assignment name expr expr', remainingTokens')
+  where
+    (expr', remainingTokens') = parse remainingTokens
+    (expr, remainingTokens) = parse tokens
 parseVariable tokens = error $ show ("Invalid variable definition", tokens)
 
 parseParameters :: [Expr] -> [Token] -> ([Expr], [Token])
 parseParameters acc [] = (acc, [])
-parseParameters acc ((RParen):tokens) = (acc, tokens)
-parseParameters acc tokens = ((acc ++ [f] ++ f'), s')
-    where
-        (f', s') = parseParameters [] s
-        (f, s) = parse(tokens)
+parseParameters acc tokens@(RParen : _) = (acc, tokens)
+parseParameters acc tokens@(BreakExpr : _) = (acc, tokens)
+parseParameters acc tokens = (acc ++ [f] ++ f', s')
+  where
+    (f', s') = parseParameters [] s
+    (f, s) = parse tokens
 
 parseCall :: [Token] -> (Expr, [Token])
-parseCall ((LiteralToken name):tokens) = (f, remainingTokens)
-    where
-        f = (Call name args)
-        (args, remainingTokens) = parseParameters [] tokens
+parseCall ((LiteralToken name) : tokens) = (f, remainingTokens)
+  where
+    f = Call name args
+    (args, remainingTokens) = parseParameters [] tokens
 parseCall tokens = error $ show ("invalid call", tokens)
 
 parsePrint :: [Token] -> (Expr, [Token])
 parsePrint tokens = (Print f, s)
-    where
-        (f, s) = parseParameters [] tokens
+  where
+    (f, s) = parseParameters [] tokens
+
+getScopeTokens :: [Token] -> Int -> [Token]
+getScopeTokens (token : tokens) currentLevel
+  | currentLevel == 0 = []
+  | token == LParen = token : getScopeTokens tokens (currentLevel + 1)
+  | token == RParen = token : getScopeTokens tokens (currentLevel - 1)
+  | otherwise = token : getScopeTokens tokens currentLevel
+getScopeTokens tokens currentLevel
+  | currentLevel == 0 = tokens
+  | otherwise = []
 
 parse :: [Token] -> (Expr, [Token])
-parse (token:tokens) = case token of
-    Let -> parseVariable tokens
-    FunctionDef -> parseFunction tokens
-    PrintToken -> parsePrint tokens
-    LParen -> parseCall tokens
-    Plus  -> parseOperator Add tokens
-    Minus -> parseOperator Sub tokens
-    Slash -> parseOperator Div tokens
-    Star  -> parseOperator Mul tokens
-    TextToken _    -> parseValue token tokens
-    LiteralToken _ -> parseValue token tokens
-    NumericToken _ -> parseValue token tokens
-    BreakExpr -> parse tokens
-    _   -> error $ show ("Trying to parse an invalid token", token)
+parse [] = (EOF, [])
+parse (token : tokens) = case token of
+  Let -> parseVariable tokens
+  FunctionDef -> parseFunction tokens
+  PrintToken -> parsePrint tokens
+  LParen -> (f, s)
+    where
+      (f, _) = parse scopeTokens
+      s = drop (length scopeTokens) tokens
+      scopeTokens = getScopeTokens tokens 1
+  TextToken _ -> parseValue token tokens
+  LiteralToken _ -> parseCall $ token : tokens
+  NumericToken _ -> parseValue token tokens
+  BreakExpr -> parse tokens
+  _ -> error $ show ("Trying to parse an invalid token", token)
