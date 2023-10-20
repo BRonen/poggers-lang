78d7962c9fe02538c20d270a60413b6163f4b178
Brenno Rodrigues <brennopereira6@gmail.com>
2023-10-20 01:37:15 -0300

    fix: updating cli to new parser

--- app/Main.hs
+++ app/Main.hs
@@ -13,49 +13,55 @@ main = do
   case length args of
     0 -> do
       print "Need to specify a poggers file path:"
-      putStrLn $ "\t./poggerslang [file path]"
+      putStrLn "\t./poggerslang [file path]"
     _ -> do
       let filePath = head args
       fileContent <- readFile filePath
 
       let tokens = tokenize fileContent
-      result <- eval (fst $ parse tokens)
-      print result
-
-{-
-putStrLn "\n---------------\n"
-print $ tokenize "let a = (x y) => + (x) (y);\nlet c = (a 1 2);\nprint c"
-print '\n'
-result <- eval $ fst $ parse $ tokenize "let a = (x y) => + (x) (y);\nlet c = a 1 2 3 (+ 2 2);\nprint c"
-print result
-print "\n lua:"
-print $ printToLua f
-putStrLn "\n---------------\n"
-
-print tokens
-print (f, s)
-print "\n\n"
-print tokens'
-print (f', s')
-print "\n\n"
-print tokens''
-print (f'', s'')
-
-r <- eval f''
-print r
-
-print $ printToLua f''
-where
+      case parse tokens of
+        (Right tree, rest) -> do
+          result <- eval tree
+          print $ show (result, rest)
+        (Left err, rest) -> print $ show (err, rest)
   {-
-  print "\n evaluation:"
-    result <- eval f
-    print "\n evaluation return:"
-    print result
+    print $ tokenize "let apply = (op a b) => op! a b; let add = (a b) => +! a b; print! (apply! add 2 3)"
+    case parse $ tokenize "let apply = (op a b) => op! a b; let add = (a b) => +! a b; print! apply! add 2 3" of
+      (Right value, a) -> do
+        print $ show (value, a)
+        r <- eval value
+        print r
+      (Left a, _) -> error $ show a
+    
+    putStrLn "\n---------------\n"
+    print $ tokenize "let a = (x y) => + (x) (y);\nlet c = (a 1 2);\nprint c"
+    print '\n'
+    print $ fst $ parse $ tokenize "let a = (x y) => + (x) (y);\nlet c = (a 1 2);\nprint c"
+    print "\n lua:"
+    --print $ printToLua f
+    putStrLn "\n---------------\n"
+
+    print tokens
+    print (f, s)
+    print "\n\n"
+    print tokens'
+    print (f', s')
+    print "\n\n"
+    print tokens''
+    print (f'', s'')
+
+    --print $ printToLua f''
+    where
+      {-
+      print "\n evaluation:"
+        result <- eval f
+        print "\n evaluation return:"
+        print result
+      -}
+      (f, s) = parse tokens
+      tokens = tokenize "[1, 2, 3, \"hello\"] a b c"
+      (f', s') = parse tokens'
+      tokens' = tokenize "print \"c\" 2 3"
+      (f'', s'') = parse tokens''
+      tokens'' = tokenize "let z = 2; let y = (a, b) => b;let x = (a, b) => print [b, a]; x \"hello\" (- 10 (+ (y 1 z) (y 1 (* (z) 2))))"
   -}
-  (f, s) = parse tokens
-  tokens = tokenize "[1, 2, 3, \"hello\"] a b c"
-  (f', s') = parse tokens'
-  tokens' = tokenize "print \"c\" 2 3"
-  (f'', s'') = parse tokens''
-  tokens'' = tokenize "let z = 2; let y = (a, b) => b;let x = (a, b) => print [b, a]; x \"hello\" (- 10 (+ (y 1 z) (y 1 (* (z) 2))))"
--}
